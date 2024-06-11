875f98395c730c8dba030f2ff863b2ec354b65d7
safa rahaf <safarahafkhan@gmail.com>
2024-06-11 19:56:44 +0600

    server app starte and connected to mongodb

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+// import globalErrorHandler from './app/middlewares/globalErrorHandle';
+// import notFound from './app/middlewares/notFound';
+// import router from './app/routes';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+// app.use('/api/v1', router);
+
+const test = async (req: Request, res: Response) => {
+  const a = "hallo Bike Rental Reservation System Backend App";
+  res.send(a);
+};
+
+app.get("/", test);
+
+// app.use(globalErrorHandler);
+
+// app.use(notFound);
+
+export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+// import { Router } from 'express';
+
+// const router = Router();
+
+// const moduleRoutes = [
+//     {
+//         path: '/bikes',
+//         route: BikeRoutes
+//     },
+//     {
+//         path: '/rental',
+//         route: RentalRoutes
+//     },
+// ]
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route))
+
+// export default router;
