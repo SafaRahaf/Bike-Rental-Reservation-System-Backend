61ec22a86bed0629f5a536e52f306d40346a1a75
safa rahaf <safarahafkhan@gmail.com>
2024-06-12 13:49:07 +0600

    creating rental crud

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -4,33 +4,38 @@ import { ROLE } from "./user.constant";
 import bcryptjs from "bcryptjs";
 import config from "../../config";
 
-const userSchema = new Schema<TUser>({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: Object.keys(ROLE),
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: 0,
-  },
-  phone: {
-    type: Number,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: Object.keys(ROLE),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   const user = this;
