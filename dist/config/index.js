119f0a6848dc2434299e657a1aaa1539c4f099b5
safa rahaf <safarahafkhan@gmail.com>
2024-06-15 18:34:22 +0600

    project is done deployment issue fixed

--- dist/config/index.js
+++ dist/config/index.js
@@ -10,14 +10,6 @@ exports.default = {
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     db_url: process.env.DB_CONNECTION,
-    salt_round: process.env.SALT_ROUND,
+    salt_round: 12,
     jwt_access_secret: process.env.JWT_SECRET,
 };
-// export default {
-//   node_env: "development",
-//   port: 3000,
-//   db_url:
-//     "mongodb+srv://assignment3:assignment3@bike-rental-service.28omdzn.mongodb.net/",
-//   salt_round: 12,
-//   jwt_access_secret: "secret",
-// };
