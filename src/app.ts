c1fd6fb3ac57ee1e919adfa28c2534ccc49396b5
safa rahaf <safarahafkhan@gmail.com>
2024-08-29 16:10:28 +0600

    project done

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,13 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(
-  cors()
-  // {
-  // cors({
-  // origin: "*",
-  //    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  // credentials: true,
-  // }
+  // cors()
+  cors({
+    origin:
+      "https://66d04829deb676e31eb734f2--loquacious-buttercream-ee212c.netlify.app/",
+    //  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
 );
 
 app.use("/api", router);
