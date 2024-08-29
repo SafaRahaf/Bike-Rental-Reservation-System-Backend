9461824e8786b21d0e4afb48356237462172760e
safa rahaf <safarahafkhan@gmail.com>
2024-08-29 13:42:23 +0600

    ready to deploy

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,12 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
+  cors()
+  // {
+  // origin: "*",
+  // origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  // credentials: true,
+  // }
 );
 
 app.use("/api", router);
